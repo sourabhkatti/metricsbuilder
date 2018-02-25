8485ab0d26a6af13f259819663252f96190dfcd2
sourabhkatti <sweetrabh@gmail.com>
2018-02-25 00:19:10 -0800

    trending vulnerability metrics

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
-from distutils.core import setup
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='MetricsBuilder',
-    version='1.0.0',
-    packages=find_packages(),
+    name='metricsbuilder',
+    version='',
+    packages=[''],
     url='',
     license='',
     author='sourabhkatti',
     author_email='',
-    description='',
-
-    install_requires=['requests']
+    description=''
 )
